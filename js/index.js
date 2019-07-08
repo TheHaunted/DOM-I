8fc22d7136e5501dd58fb4c17ce36f0b712eb209
TheHaunted <kevin_lopez@live.com>
2019-07-08 15:52:34 -0500

    added two nav items

--- js/index.js
+++ js/index.js
@@ -50,11 +50,20 @@ nav.forEach(function(a){
   a.style.color = 'green';
 });
 
-// Bookmark----------------------------------------------------
 let navParent = document.querySelector('nav');
-navParent.prepend(a);
-navParent.append(a);
-console.log(navParent);
+
+let navMemes = document.createElement("a");
+navMemes.href = "#";
+navMemes.textContent = "Memes";
+navMemes.style.color = 'green';
+
+let navFAQ = document.createElement('a');
+navFAQ.href = '#';
+navFAQ.textContent = 'FAQ';
+navFAQ.style.color = 'green';
+
+navParent.appendChild(navMemes);
+navParent.prepend(navFAQ);
 
 let cta = document.querySelector(".cta-text h1");
 cta.textContent = siteContent["cta"]["h1"];
