6b600380b4199d316e5650ad14f81bff5bfb7a09
TheHaunted <kevin_lopez@live.com>
2019-08-05 15:26:11 -0500

    finished task 1

--- js/index.js
+++ js/index.js
@@ -40,3 +40,12 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+let codeCircle = document.querySelector('#cta-img');
+codeCircle.setAttribute('src', siteContent["cta"]["img-src"]);
+
+
+let middleImg = document.querySelector('.middle-img');
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
